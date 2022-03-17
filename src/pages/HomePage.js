409ff6f67a2fcac4ff4c384feb966ebf5c9f861c
thebranflakes <73903526+thebranflakes@users.noreply.github.com>
2022-03-16 20:48:57 -0400

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ function HomePage() {
     return (
         <>
         <article className="App-article">
-            <h2>Random Movie Generator</h2>
+            <h2>Final Portfolio Project</h2>
             <p>Hello, welcome to my portfolio project for QS 262. Here you'll find a random movie generator and pictures of my magazine of films currently on my watchlist.</p>
             <p>I'm a computer science major and I love film. The Media Analysis was my favorite assignment we had this quarter so I wanted to sort of integrate all these parts together for the final.</p>
             <p>A lot of who I am, has come from watching an endless amount of movies. As I've gotten older I've been able to get into movies that are made and function outside of mainstream media and it's really enriched my life. The artform of film is a profound way to tell stories from numerous voices and is a beuatiful way to expand your world.</p>
